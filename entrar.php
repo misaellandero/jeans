c04e9a3593caf83592bf2654f10cbd6b13009e4e
misaellandero <misaellandero@landercorp.com.mx>
2018-05-28 13:56:40 -0500

    Se añadieron como locales los archivos

--- entrar.php
+++ entrar.php
@@ -145,7 +145,8 @@ $totalRows_consulta_usuario = mysql_num_rows($consulta_usuario);
     <title>Inventario Producto terminado </title>
 
 	<link rel="stylesheet" type="text/css" href="styles/style.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="styles/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="scripts/DataTables/datatables.css"/>
   <script src="scripts/jquery.js"></script>
 
@@ -160,7 +161,7 @@ $totalRows_consulta_usuario = mysql_num_rows($consulta_usuario);
   <script src="scripts/jquery-barcode.js"></script>
   <script src="scripts/jquery-impromptu.js"></script>
 
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  <script src="styles/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
   <!--iOS -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
